7fd38b0a39c0213b06b39e51dccdcc39fc17a47b
NOOR RAIHAN <blazerred71@gmail.com>
2022-08-18 20:53:39 +0800

    update

--- cert_gen.py
+++ cert_gen.py
@@ -1,10 +1,14 @@
+from asyncio.windows_events import NULL
 from PIL import Image, ImageFont, ImageDraw
+from numpy import true_divide
 import pandas
 import os.path
 import os
+import coordinate
 from os import path
 import time 
 import json
+import sys
 
 banner = ''' 
   ______            _____          _   
@@ -14,123 +18,244 @@ banner = '''
  | |____ / /| |_| | |___|  __/ |  | |_ 
  |______/___|\__, |\_____\___|_|   \__|
               __/ |                    
-             |___/                     
+             |___/          
+              
+              V2.0           
         AUTHOR : NOOR RAIHAN
           
     '''
 
 choice = '''
+[1] Generate Multiple E-Certificate
+[2] Generate Single E-Certificate
+[3] Configurations
+[4] Exit
+'''
+choiceMultiple = '''
 [1] Generate E-Certificate
-[2] Double Checking E-Certificate
-[3] Generate single E-Certificate
-[4] Adding Signature to Certificate (in development)
-[5] Generate E-certification (with IC)
-[6] Generate single E-certification (with IC)
-[7] Set new coordinate
+[2] Generate E-certification (with IC)
+[3] Back
 '''
 
-print(banner)
-print(choice)
+choiceSingle = '''
+[1] Generate single E-Certificate
+[2] Generate single E-certification (with IC)
+[3] Back
+'''
 
-dir_check2 = False
-while dir_check2 == False:
-  ex_file = input("Enter excel filepath (.xlsx): ")
-  dir_check2 = path.exists(ex_file)
-  if dir_check2 == False:
-    print("\033[1;31;40mFile does not exist \033[1;37;40m")
+choiceConfig ='''
+[1] Reposition
+[2] Choose font
+[3] Back
+'''
 
-    
-name = pandas.read_excel(ex_file)
-name_list = name['Name'].tolist()
+#list of globals variables
+excel_file = NULL
+name = NULL
+name_list = []
+x = 0
+y = 0
+fontsize = 0
+fontname = NULL
+cert_path = NULL
+
+def menu_Multiple():
+  os.system('cls')
+  print(banner)
+  print(choiceMultiple)
+  inp = int(input('Choose: '))
+
+  flag = True
+
+  while (flag):
+    if inp == 1:
+      generate_cert()
+      break
+    elif inp == 2: 
+      generateWithIC()
+      break
+    elif inp == 3:
+      menu()
+      break
+    elif inp == 4:
+      sys.exit(0)
+    else:
+      print('Invalid input')
+
+def menu_Single():
+  os.system('cls')
+  print(banner)
+  print(choiceSingle)
+  inp = int(input('Choose: '))
+
+  flag = True
+
+  while (flag):
+    if inp == 1:
+      singleGenerate()
+      break
+    elif inp == 2: 
+      singleGenerateWithIC()
+      break
+    elif inp == 3:
+      menu()
+      break
+    else:
+      print('Invalid input')
+
+def menu_Config():
+  os.system('cls')
+  print(banner)
+  print(choiceConfig)
+  inp = int(input('Choose: '))
+
+  flag = True
+
+  while (flag):
+    if inp == 1:
+      coordinate.main()
+      os.system('pause')
+      menu()
+      break
+    elif inp == 2: 
+      config_Font()
+      break
+    elif inp == 3:
+      menu()
+      break
+    else:
+      print('Invalid input')
+
+def config_Font():
+  counter = 0
+  path = 'bin/fonts/'
+  listFont = os.listdir(path)
+  for font in listFont:
+    print("["+str(counter + 1)+"]", font)
+    counter += 1
+  
+  inp = int(input('Choose font: '))
+  f_index = inp - 1
+
+  with open('coordinate.json') as jsonfile:
+      datas = json.load(jsonfile)
+  for data in datas['coordinate']:
+      data['font'] = path + listFont[f_index]
+
+  with open('coordinate.json','w') as output:
+      json.dump(datas, output)
+  print("Successfully save the font!")
+  os.system('pause')
+  menu()
+
+
+#get the excel and file
+def get_File():
+  global excel_file, name, name_list
+
+  dir_check2 = False
+  while dir_check2 == False:
+    ex_file = input("Enter excel filepath (.xlsx): ")
+    dir_check2 = path.exists(ex_file)
+    if dir_check2 == False:
+      print("\033[1;31;40mFile does not exist \033[1;37;40m")
+  
+  try:
+    name = pandas.read_excel(ex_file)
+    name_list = name['Name'].tolist()
+  except Exception:
+    print("Can't find Name in file")
 
 #read JSON file to get coordinate
 def readJSON():
-  with open('coordinate.json') as jsonfile:
-    data = json.load(jsonfile)
-    for dat in data['coordinate']:
-      x = dat['x-axis']
-      y = dat['y-axis']
-      fontsize = dat['fontsize']
-  
-  return x, y, fontsize
+  global x, y, fontsize, fontname
 
-def generateWithIC(x,y,fontsize):
+  with open('coordinate.json') as jsonfile:
+    datas = json.load(jsonfile)
+    for data in datas['coordinate']:
+      x = int(data['x-axis'])
+      y = int(data['y-axis'])
+      fontsize = int(data['fontsize'])
+      fontname = data['font']
+  
+  return x, y, fontsize, fontname
+
+def get_Template():
+  global cert_path
   dir_check = False
-  ic_list = name['IC'].tolist()
 
   while dir_check == False:
-      cert_path = input("Enter your template path: ")
+      cert_path = input("Enter your template path [must be in .jpg or .png]: ")
       dir_check = path.exists(cert_path)
       if dir_check == False:
         print("\033[1;31;40mFile does not exist \033[1;37;40m")
-  
-  
+
+#generate certificate with IC
+def generateWithIC():
+  global name, name_list
+  get_File()
+  get_Template()
+  readJSON()
+
   filepath = input("Save to (folder): ")
-  o_folder = os.system("mkdir " + filepath)
+  try:
+    ic_list = name['IC'].tolist()
+  except Exception:
+    print("IC can not be found in the file")
+
   print("\033[1;33;40mTotal name : " + str(len(name_list)) + "\033[1;37;40m")
   time.sleep(5)
+  
+  filepath = "outputs/" + filepath
+  
+  try:
+    os.makedirs(filepath, exist_ok = True)
+  except OSError as err:
+    print('Directory', filepath, "can not be created")
 
   for i, z in zip(name_list, ic_list):
-    z = str(z)
-    cert = Image.open(cert_path)
-    draw = ImageDraw.Draw(cert)
-    font = ImageFont.truetype("arial.ttf",fontsize)
-    w,h = font.getsize(i)
-    w2,h2 = font.getsize(z)
-    draw.text((x,y), i, (0,0,0), anchor="mm",font=font)
-    draw.text((x,(y+(y*0.1))), "("+z+")", (0,0,0), anchor="mm",font=font)
-    print("\033[1;32;40mGenerating certificate for",i,"\033[1;37;40m")
-    cert.save(filepath + "/cert_" + i + ".pdf")
+    cert_generateIC(i,z,filepath)
   os.system('pause')
+  menu()
 
-def singleGenerateWithIC(x,y,fontsize): #for generate single certificate
-  dir_check = False
+
+#for generate single certificate
+def singleGenerateWithIC():
+  get_Template()
+  readJSON()
+
   name = input("\nFullname: ")
-  ic = input("\IC Number: ")
-  while dir_check == False:
-      cert_path = input("Enter your template path: ")
-      dir_check = path.exists(cert_path)
-      if dir_check == False:
-        print("\033[1;31;40mFile does not exist \033[1;37;40m")
-  
-  cert = Image.open(cert_path)
-  draw = ImageDraw.Draw(cert)
-  font = ImageFont.truetype("arial.ttf",fontsize)
-  w,h = font.getsize(name)
-  w2, h2 = font.getsize(ic)
-  draw.text((x,y), name, (0,0,0), anchor="mm",font=font)
-  draw.text((x,y+(y*0.1)), "("+str(ic)+")", (0,0,0), anchor="mm",font=font)
-  print("\033[1;32;40mGenerating certificate for",name,"\033[1;37;40m")
-  cert.save("cert_" + name + ".pdf")
+  ic = input("IC Number: ")
+  filepath = "outputs"
+  cert_generateIC(name, ic, filepath)
   os.system('pause')
+  menu()
 
+#for generating multiple certificate
+def generate_cert():
+  get_File()
+  get_Template()
+  readJSON()
 
-def generate_cert(x,y,fontsize): #for generating multiple certificate
-  dir_check = False
-
-  while dir_check == False:
-      cert_path = input("Enter your template path: ")
-      dir_check = path.exists(cert_path)
-      if dir_check == False:
-        print("\033[1;31;40mFile does not exist \033[1;37;40m")
-  
   filepath = input("Save to (folder): ")
-  o_folder = os.system("mkdir " + filepath)
   print("\033[1;33;40mTotal name : " + str(len(name_list)) + "\033[1;37;40m")
   time.sleep(5)
   
+  filepath = "outputs/" + filepath
+  
+  try:
+    os.makedirs(filepath, exist_ok = True)
+  except OSError as err:
+    print('Directory', filepath, "can not be created")
+
   for i in name_list:
-      cert = Image.open(cert_path)
-      draw = ImageDraw.Draw(cert)
-      font = ImageFont.truetype("arial.ttf",fontsize)
-      w,h = font.getsize(i)
-      draw.text((x,y), i.upper(), (0,0,0), anchor="mm",font=font)
-      print("\033[1;32;40mGenerating certificate for",i,"\033[1;37;40m")
-      cert.save(filepath + "/cert_" + i + ".pdf")
+    cert_generate(i, filepath)
+
   os.system('pause')
+  menu()
 
-
-def check_cert(): #for checking all the cert based on excel namelist
+#for checking all the cert based on excel namelist
+def check_cert():
   dir_check = False
 
   while dir_check == False:
@@ -150,56 +275,108 @@ def check_cert(): #for checking all the cert based on excel namelist
         miss +=1
   print("\n",miss, "file missing")
   os.system('pause')
+  menu()
 
-def singleGenerate(x,y,fontsize): #for generate single certificate
-  dir_check = False
+#for generate single certificate
+def singleGenerate():
+  get_Template()
+  readJSON()
+
   name = input("\nFullname: ")
-  while dir_check == False:
-      cert_path = input("Enter your template path: ")
-      dir_check = path.exists(cert_path)
-      if dir_check == False:
-        print("\033[1;31;40mFile does not exist","\033[1;37;40m")
-  
-  cert = Image.open(cert_path) 
-  draw = ImageDraw.Draw(cert)
-  font = ImageFont.truetype("arial.ttf",fontsize)
-  w,h = font.getsize(name)
-  draw.text((x,y), name, (0,0,0), anchor="mm",font=font)
-  print("\033[1;32;40mGenerating certificate for",name,"\033[1;37;40m")
-  cert.save("cert_" + name + ".pdf")
+  filepath = "outputs"
+  cert_generate(name,filepath)
   os.system('pause')
+  menu()
 
-def addSignature(): #adding a signature
-  dir_check = False
-  while dir_check == False:
-      cert_path = input("Enter your template path: ")
-      dir_check = path.exists(cert_path)
-      if dir_check == False:
-        print("\033[1;31;40mFile does not exist \033[1;37;40m")
+#algortihm to generate the certificate
+def cert_generate(name, filepath):
+  global x,y,fontname,fontsize,cert_path
 
-  foreground = Image.open("logotest.png")
-  background = Image.open(cert_path)
-  newf = foreground.resize((1200,1200), Image.ANTIALIAS)
-  background.paste(newf,(1000,0), newf)
-  print("\033[1;32;40mAdding Signature to Certificate\033[1;37;40m")
-  background.save("new_signature_template.jpg")
+  name = name.upper()
+  new_name = name_sanitize(name)
+  new_name = new_name.upper()
+
+  try:
+    cert = Image.open(cert_path)
+    draw = ImageDraw.Draw(cert)
+    font = ImageFont.truetype(fontname,fontsize)
+    w,h = font.getsize(name)
+    draw.text((x,y), name.upper(), (0,0,0), anchor="mm",font=font)
+    print("\033[1;32;40mGenerating certificate for",name,"\033[1;37;40m")
+    cert.save(filepath + "/cert_" + new_name + ".pdf")
+  except Exception as err:
+    print("Failed generate: ", err)
 
 
-x,y,fontsize = readJSON()
+#algortihm to generate the certificate with IC
+def cert_generateIC(name, ic, filepath):
+  global x,y,fontname,fontsize,cert_path
 
-u_choice = int(input("Choice: "))
-if u_choice == 1:
-  generate_cert(x,y,fontsize)
-elif u_choice == 2:
-  check_cert()
-elif u_choice == 3:
-  singleGenerate(x,y,fontsize)
-elif u_choice == 4:
-  addSignature()
-elif u_choice == 5:
-  generateWithIC(x,y,fontsize)
-elif u_choice == 6:
-  singleGenerateWithIC(x,y,fontsize)
-elif u_choice == 7:
-  os.system('.\coordinate.exe')
+  name = name.upper()
+  new_name = name_sanitize(name)
+  new_name = new_name.upper()
+  ic = ic_sanitize(ic)
+
+  try:
+      cert = Image.open(cert_path)
+      draw = ImageDraw.Draw(cert)
+      font = ImageFont.truetype(fontname,fontsize)
+      w,h = font.getsize(name)
+      w2,h2 = font.getsize(ic)
+      draw.text((x,y), name, (0,0,0), anchor="mm",font=font)
+      draw.text((x,(y+(y*0.1))), ic, (0,0,0), anchor="mm",font=font)
+      print("\033[1;32;40mGenerating certificate for",name,"\033[1;37;40m")
+      cert.save(filepath + "/cert_" + new_name + ".pdf")
+  except Exception as err:
+      print("Failed generate: ", err)
+  
+def name_sanitize(name):
+  #sanitizing the indian name that lead to bug
+  name = name.upper()
+  if "A/L" in name: 
+    name = name.replace("A/L", "")
+  elif "A/P" in name:
+    name = name.replace("A/P", "")
+  
+  return name
+
+def ic_sanitize(ic):
+  #sanitizing the ic by adding - 
+  if "-" not in ic:
+    new_ic = ic[0:6] + "-" + ic[6:8] + "-" + ic[8:12]
+  else:
+    new_ic = ic
+  
+  return new_ic
+
+#adding a signature
+# def addSignature():
+#   dir_check = False
+#   while dir_check == False:
+#       cert_path = input("Enter your template path: ")
+#       dir_check = path.exists(cert_path)
+#       if dir_check == False:
+#         print("\033[1;31;40mFile does not exist \033[1;37;40m")
+
+#   foreground = Image.open("logotest.png")
+#   background = Image.open(cert_path)
+#   newf = foreground.resize((1200,1200), Image.ANTIALIAS)
+#   background.paste(newf,(1000,0), newf)
+#   print("\033[1;32;40mAdding Signature to Certificate\033[1;37;40m")
+#   background.save("new_signature_template.jpg")
+
+def menu():
+  os.system('cls')
+  print(banner)
+  print(choice)
+  u_choice = int(input("Choice: "))
+  if u_choice == 1:
+    menu_Multiple()
+  elif u_choice == 2:
+    menu_Single()
+  elif u_choice == 3:
+    menu_Config()
+
+if __name__ == "__main__":
+  menu()
 
--- coordinate.py
+++ coordinate.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from tkinter import *
 from tkinter import ttk
 import PIL
@@ -8,48 +9,51 @@ import os.path
 from os import path
 import json
 
-dir_check = False
-while dir_check == False:
-      image_path = input("Enter your template path: ")
-      dir_check = path.exists(image_path)
-      if dir_check == False:
-        print("\033[1;31;40mFile does not exist \033[1;37;40m")
-
-
-root = Tk()
-img = Image.open(image_path)
-width, height = img.size
-width = int(width/4)
-height = int(height/4)
-img = img.resize((width,height), Image.ANTIALIAS)
-image = ImageTk.PhotoImage(img)
+#list of variables
+x = 0
+y = 0
+fontsize = 0
+in_width = 0
+in_height = 0
+image_path = NULL
+root = NULL
 
 
 
-def writeJson(x,y,fontsize):
-    data = {}
-    data['coordinate'] = []
-    data['coordinate'].append({
-        'x-axis' : x,
-        'y-axis' : y, 
-        'fontsize' : fontsize
-    })
+def writeJson():
+    global root
+
+    with open('coordinate.json') as jsonfile:
+        datas = json.load(jsonfile)
+    for data in datas['coordinate']:
+        data['x-axis'] = str(data['x-axis']).replace(str(data['x-axis']), str(x))
+        data['y-axis'] = str(data['y-axis']).replace(str(data['y-axis']), str(y))
+        data['fontsize'] = str(data['fontsize']).replace(str(data['fontsize']), str(fontsize))
+        data['font'] = data['font']
+
     with open('coordinate.json','w') as output:
-        json.dump(data, output)
-    print("Successfully save the coordinate!")
+        json.dump(datas, output)
+    print("Successfully save the position!")
     root.destroy()
 
 
 def getCoordinate(event):
-    x = event.x * 4
-    y = event.y * 4
+    global x, y, in_height, in_width
+
+    if(in_width > 1040 and in_height > 1040):
+        x = event.x * 4
+        y = event.y * 4
+    else:
+        x = event.x
+        y = event.y
     print(str(x)+","+str(y))
-    singleGenerate(x, y)
+    singleGenerate()
     
-def singleGenerate(x,y): #for generate single certificate
-    dir_check = False
-    test = "THIS IS A TEST NAME FOO GG BOII"
-    test2 = "00000000000"
+def singleGenerate():
+    global fontsize, image_path
+
+    test = "JOHN SMITH BIN AKHLAKEN"
+    test2 = "000000-00-0000"
     cert = Image.open(image_path)
     draw = ImageDraw.Draw(cert)
     fontsize = 1
@@ -61,22 +65,45 @@ def singleGenerate(x,y): #for generate single certificate
 
     
     fontsize = int(fontsize/2)
-    font = ImageFont.truetype("arial.ttf",fontsize)
+    font = ImageFont.truetype("bin/fonts/arial.ttf",fontsize)
     draw.text((x,y), test, (0,0,0),anchor="mm",font=font)
     draw.text((x,(y+(y*0.1))), "("+str(test2)+")", (0,0,0), anchor="mm",font=font)
     cert.show()
-    ask = input("confirm? [y/n]: ")
-    if ask == "y":
-        writeJson(x, y, fontsize)
-    else:
-        pass
 
-canv = Canvas(root,width=str(width), height=str(height))
-canv.pack()
-canv.create_image(0,0,image=image, anchor=NW)
-canv.bind('<Button-1>',getCoordinate)
+def main():
+    global in_width, in_height, image_path, root
 
-root.mainloop()
+    dir_check = False
+    while dir_check == False:
+        image_path = input("Enter your template path: ")
+        dir_check = path.exists(image_path)
+        if dir_check == False:
+            print("\033[1;31;40mFile does not exist \033[1;37;40m")
+
+
+    root = Tk()
+    root.title('Positioning')
+    img = Image.open(image_path)
+    width, height = img.size
+    in_width = width
+    in_height = height
+    print('Image Resolution:', width, height)
+    if(width > 1040 and height > 1040):
+        width = int(width/4)
+        height = int(height/4)
+        img = img.resize((width,height), Image.ANTIALIAS)
+
+    image = ImageTk.PhotoImage(img)
+
+    ttk.Button(root, text="Save Position", command=writeJson).pack()
+    canv = Canvas(root,width=str(width), height=str(height))
+    canv.pack()
+    canv.create_image(0,0,image=image, anchor=NW)
+    canv.bind('<Button-1>',getCoordinate)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
 
 
 
